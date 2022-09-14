87638e2cadb838545fb83316679ca3d1939d304f
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-14 18:54:28 +0500

    Update Zadacha 21 14.09.2022 Reviuer

--- Zadacha 21/Program.cs
+++ Zadacha 21/Program.cs
@@ -12,47 +12,31 @@ string what_coordinate(int coordinate) // Координаты X=1 Y=2 Z=3 4....
     }
 };
 
-double[] coordinats1Point = new double[3];
-double[] coordinats2Point = new double[3];
-
 Console.Clear();
 
-for (int iCountPoint=0; iCountPoint<2; iCountPoint++)      // Колличество точек     = iCountPoint
-{
-    double coordinateXYZ;
-    for (int jCountCoor=0; jCountCoor<3; jCountCoor++) // Колличество координат = jCountCoor
-    {
-        Console.Write($"Введите координату {what_coordinate(coordinate:jCountCoor)} для точки Point{iCountPoint+1}: ");
-        switch(iCountPoint)
-        {
-            case 0:
-            {
-                coordinats1Point[jCountCoor] = double.Parse(Console.ReadLine());
-                Console.Write($"Point{iCountPoint+1} X:{coordinats1Point[0]} Y:{coordinats1Point[1]} Z:{coordinats1Point[2]}\n");
-                break;
-            }
-            
-            case 1:
-            {
-                coordinats2Point[jCountCoor] = double.Parse(Console.ReadLine());
-                Console.Write($"Point{iCountPoint+1} X:{coordinats2Point[0]} Y:{coordinats2Point[1]} Z:{coordinats2Point[2]}\n");
-                break;
-            }
+int points = 2;
+int coorNum = 3;
 
-            default:
-                break;
-        }
+int[,] coordinates = new int [points, coorNum];
+for (int k = 0; k < points; k++)
+{
+    string txt = k == 0 ? "Точка A" : "Точка B";
+    Console.WriteLine(txt);
+    for (int m = 0; m < coorNum; m++)
+    {
+        Console.WriteLine($"Введите значение {m + 1}-й координаты");
+        coordinates[k, m] = int.Parse(Console.ReadLine());
     }
 }
 
 Console.Clear();
-Console.Write($"Point1 X:{coordinats1Point[0]} Y:{coordinats1Point[1]} Z:{coordinats1Point[2]}\n");
-Console.Write($"Point2 X:{coordinats2Point[0]} Y:{coordinats2Point[1]} Z:{coordinats2Point[2]}\n");
+Console.Write($"Point1 X:{coordinates[0,0]} Y:{coordinates[0,1]} Z:{coordinates[0,2]}\n");
+Console.Write($"Point2 X:{coordinates[1,0]} Y:{coordinates[1,1]} Z:{coordinates[1,2]}\n");
 
-double coordinats_X2_X1 = coordinats2Point[0] - coordinats1Point[0];
-double coordinats_Y2_Y1 = coordinats2Point[1] - coordinats1Point[1];
-double coordinats_Z2_Z1 = coordinats2Point[2] - coordinats1Point[2];
+double coordinats_X2_X1 = coordinates[1,0] - coordinates[0,0];
+double coordinats_Y2_Y1 = coordinates[1,1] - coordinates[0,1];
+double coordinats_Z2_Z1 = coordinates[1,2] - coordinates[0,2];
 double sqrt_PowX2_PowY2_PowZ2 = Math.Sqrt(Math.Pow(coordinats_X2_X1,2)+Math.Pow(coordinats_Y2_Y1,2)+Math.Pow(coordinats_Z2_Z1,2));
-
+sqrt_PowX2_PowY2_PowZ2 =  Math.Round(sqrt_PowX2_PowY2_PowZ2, 2);
 
 Console.WriteLine($"Расстояние между Ponit1 и Point2: {sqrt_PowX2_PowY2_PowZ2}");
