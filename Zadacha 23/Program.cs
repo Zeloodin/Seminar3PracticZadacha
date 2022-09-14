52843cac11f101681a289b7bdf72e4dc906ce486
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-14 18:28:28 +0500

    Update Zadacha 23 14.09.2022 Reviuer

--- Zadacha 23/Program.cs
+++ Zadacha 23/Program.cs
@@ -8,27 +8,12 @@ try
     string inputString = Console.ReadLine();
     double numberN = Convert.ToDouble(inputString);
     double[] massivCubovChisel = new double[Convert.ToUInt64(numberN)];
-    bool massivBool = false;
 
     for (int i = 0; i < numberN; i++)
     {
-        massivCubovChisel[i] = Convert.ToDouble(Math.Pow(i,2));
-        if (i >= numberN-1) massivBool = true;
+        massivCubovChisel[i] = Convert.ToDouble(Math.Pow(i+1,3));
     }
-    
-    if (massivBool)
-    {
-        Console.WriteLine(string.Join(", ", massivCubovChisel));
-
-        // Console.WriteLine(String.Concat<double>(massivCubovChisel));
-
-        // int lenghtMax = massivCubovChisel.Length;
-        // for (int i = 0; i < lenghtMax; i++)
-        // {
-        //     Console.Write($"{massivCubovChisel[i]} ");
-        // }
-    }
-    
+    Console.WriteLine(string.Join(", ", massivCubovChisel));
 }
 catch
 {
